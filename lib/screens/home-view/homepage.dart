ac7823b58e80ed6cdf72a9b7daf3c92600f4dcaa
Ryan <ryan.camilleri4@gmail.com>
2020-04-23 11:11:37 +0200

    Implemented alert dialog to go to tutor profile from homepage tuition.

--- lib/screens/home-view/homepage.dart
+++ lib/screens/home-view/homepage.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:tutorsplus/models/user.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:tutorsplus/models/tuition.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:tutorsplus/screens/home-view/tuition_tile.dart';
 import 'package:tutorsplus/services/database.dart';
 import 'package:tutorsplus/shared/loading.dart';
 import '../../shared/common.dart';
 
 class Home extends StatefulWidget {
-
   final UserData userData;
   Home({this.userData});
 
@@ -21,43 +22,82 @@ class _HomeState extends State<Home> {
   double width = 0;
 
   int _current = 0;
-  int _timeDay = 0;
   bool tuitionsToggle = false;
-  List <Tuition> tuitions = List<Tuition>();
+  List<Tuition> tuitions = List<Tuition>();
 
-  void _timeDayCheck() {
-    var hour = DateTime.now().hour;
-    setState(() {
-      if (hour < 12) {
-        _timeDay = 1;
-      } else if (hour < 17) {
-        _timeDay = 2;
-      }else{
-        _timeDay = 3;
+  // void _timeDayCheck() {
+  //   var hour = DateTime.now().hour;
+  //   setState(() {
+  //     if (hour < 12) {
+  //       _timeDay = 1;
+  //     } else if (hour < 17) {
+  //       _timeDay = 2;
+  //     } else {
+  //       _timeDay = 3;
+  //     }
+  //   });
+  // }
+
+  void populateTuitions() {
+    Firestore.instance.collection('tuition').getDocuments().then((docs) {
+      if (docs.documents.isNotEmpty) {
+        setState(() {
+          tuitionsToggle = true;
+        });
+        for (int i = 0; i < docs.documents.length; ++i) {
+          String tuitionId = LatLng(
+                  docs.documents[i].data['tuition_geopoint'].latitude,
+                  docs.documents[i].data['tuition_geopoint'].longitude)
+              .toString();
+          Tuition newTuition = new Tuition(
+            // tuitionId,
+            // docs.documents[i].data['tuition_title'],
+            // docs.documents[i].data['tutor_name'],
+            // docs.documents[i].data['tuition_category'],
+            // docs.documents[i].data['tuition_level'],
+            // docs.documents[i].data['isPremium'],
+            // docs.documents[i].data['locality'],
+            // docs.documents[i].data['tuition_geopoint'].longitude,
+            // docs.documents[i].data['tuition_geopoint'].latitude);
+            id: tuitionId,
+            isPremium: docs.documents[i].data['tuition_isPremium'],
+            isOnline: docs.documents[i].data['tuition_isOnline'],
+            category: docs.documents[i].data['tuition_category'],
+            level: docs.documents[i].data['tuition_level'],
+            name: docs.documents[i].data['tuition_name'],
+            tutor: docs.documents[i].data['tuition_tutor'],
+            description: docs.documents[i].data['description'],
+            locality: docs.documents[i].data['tuition_locality'],
+            tutorRef: docs.documents[i].data['tuition_tutorRef'],
+            latitude: docs.documents[i].data['tuition_geopoint'].latitude,
+            longitude: docs.documents[i].data['tuition_geopoint'].longitude,
+          );
+          tuitions.add(newTuition);
+        }
       }
     });
   }
 
   void initState() {
     super.initState();
-    _timeDayCheck();
+    // _timeDayCheck();
+    populateTuitions();
   }
 
   Widget _carouselSlider(BuildContext context) {
-    
     final List<String> imgList = [
-      'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+      'https://hillelontario.org/uoft/files/2019/08/WelcomeBanners-UofT-1-1080x675.png',
+      'https://www.schoolfeesgroup.ae/wp-content/uploads/2018/12/AdobeStock_88074933.jpg',
+      'https://d19d5sz0wkl0lu.cloudfront.net/dims4/default/b2928d2/2147483647/resize/800x%3E/quality/90/?url=https%3A%2F%2Fatd-brightspot.s3.amazonaws.com%2F7f%2F38%2Fb37d0d6148e48fea8f76209eb3bb%2Fbigstock-pretty-teacher-smiling-at-came-69887626-1.jpg',
+      'https://cdn.mos.cms.futurecdn.net/gmbHWBEMwL3nZYJ9jRYPC.jpg',
       'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
-      'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
-      'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
-      'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
-      'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+      'https://previews.123rf.com/images/goodstudio/goodstudio1807/goodstudio180700503/105507176-horizontal-banner-with-stationery-supplies-and-accessories-for-lessons-items-for-education-back-to-s.jpg'
     ];
 
     final List<Widget> imageSliders = imgList
         .map((item) => Container(
               child: Container(
-                margin: EdgeInsets.symmetric(horizontal:2.0, vertical: 2.0),
+                margin: EdgeInsets.symmetric(horizontal: 2.0, vertical: 2.0),
                 child: ClipRRect(
                     borderRadius: BorderRadius.all(Radius.circular(5.0)),
                     child: Stack(
@@ -81,7 +121,7 @@ class _HomeState extends State<Home> {
                             padding: EdgeInsets.symmetric(
                                 vertical: 10.0, horizontal: 20.0),
                             child: Text(
-                              'New offers on laptops at this store! (Sponsored)',
+                              'Image ${imgList.indexOf(item)+1}',
                               style: TextStyle(
                                 color: Colors.white,
                                 fontSize: 15.0,
@@ -96,8 +136,7 @@ class _HomeState extends State<Home> {
             ))
         .toList();
 
-    return Column(
-    children: <Widget>[
+    return Column(children: <Widget>[
       CarouselSlider(
         items: imageSliders,
         options: CarouselOptions(
@@ -115,7 +154,7 @@ class _HomeState extends State<Home> {
             }),
       ),
       Container(
-        height: height*0.06,
+        height: height * 0.06,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: imgList.map((url) {
@@ -136,164 +175,171 @@ class _HomeState extends State<Home> {
   }
 
   Widget _loadNewTuition(UserData userData, List<Tuition> tuitionList) {
-
     List<String> userInterests = userData.interests;
+    bool interestedTuition = false;
+
+    for (int i = 0; i < tuitionList.length; ++i) {
+      if (userInterests.contains(tuitionList[i].category)) {
+        interestedTuition = true;
+        break;
+      }
+    }
 
     return ListView.builder(
-      padding: EdgeInsets.fromLTRB(0,0,0,10),
-      itemCount: tuitionList.length,
-      itemBuilder: (context, index){
-
-        Tuition tuition = tuitionList[index];
-        if(userInterests.contains(tuition.category)){
-          return TuitionTile(tuition: tuition, index: index,);
-        }else{
-          return SizedBox();
-        }
-
-    });
+        padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+        itemCount: interestedTuition ? tuitionList.length : (tuitionList.length<5 ? tuitionList.length : 5),
+        itemBuilder: interestedTuition
+            ? (context, index) {
+                Tuition tuition = tuitionList[index];
+                if (userInterests.contains(tuition.category)) {
+                  interestedTuition = true;
+                  return TuitionTile(
+                    tuition: tuition,
+                    index: index,
+                  );
+                } else {
+                  return SizedBox();
+                }
+              }
+            : (context, index) {
+                Tuition tuition = tuitionList[index];
+                return TuitionTile(tuition: tuition, index: index);
+              });
   }
 
   @override
   Widget build(BuildContext context) {
-
     height = MediaQuery.of(context).size.height;
     width = MediaQuery.of(context).size.width;
 
     return StreamBuilder<List<Tuition>>(
-      stream: DatabaseService().tuitionList,
-      builder: (context, snapshot) {
-        if(snapshot.hasData){
-          
-          List<Tuition> tuitionList = snapshot.data;
+        stream: DatabaseService().tuitionList,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List<Tuition> tuitionList = snapshot.data;
 
-          return Container(
-            height: double.infinity,
-            width: double.infinity,
-            color: blackPlus,
+            return Container(
+              height: double.infinity,
+              width: double.infinity,
+              color: blackPlus,
+              child: Column(
+                children: <Widget>[
+                  // SizedBox(height: 30),
 
-            child: Column(
-              children: <Widget>[
+                  // Container(
+                  //     width: width* 0.9,
+                  //     height: height * 0.1,
+                  //     decoration: BoxDecoration(
+                  //       borderRadius: BorderRadius.circular(30.0),
+                  //       color: whitePlus,
+                  //       border: Border.all(
+                  //         color: greyPlus,
+                  //         width: 1.5,
+                  //       ),
+                  //     ),
+                  //     child: Center(
+                  //         child: _timeDay == 1
+                  //             ? Text(
+                  //                 'Good Morning! 🌞',
+                  //                 style: TextStyle(
+                  //                   color: blackPlus,
+                  //                   fontWeight: FontWeight.bold,
+                  //                   fontSize: 38,
+                  //                 ),
+                  //               )
+                  //             : _timeDay == 2
+                  //                 ? Text(
+                  //                     'Good Afternoon! ☀️',
+                  //                     style: TextStyle(
+                  //                       color: blackPlus,
+                  //                       fontWeight: FontWeight.bold,
+                  //                       fontSize: 38,
+                  //                     ),
+                  //                   )
+                  //                 : Text(
+                  //                     'Good Evening! 🌙',
+                  //                     style: TextStyle(
+                  //                       color: blackPlus,
+                  //                       fontWeight: FontWeight.bold,
+                  //                       fontSize: 38,
+                  //                     ),
+                  //                   ))),
 
-                // SizedBox(height: 30),
-                
-                // Container(
-                //     width: width* 0.9,
-                //     height: height * 0.1,
-                //     decoration: BoxDecoration(
-                //       borderRadius: BorderRadius.circular(30.0),
-                //       color: whitePlus,  
-                //       border: Border.all(
-                //         color: greyPlus,
-                //         width: 1.5,
-                //       ),              
-                //     ),
-                //     child: Center(
-                //         child: _timeDay == 1
-                //             ? Text(
-                //                 'Good Morning! 🌞',
-                //                 style: TextStyle(
-                //                   color: blackPlus,
-                //                   fontWeight: FontWeight.bold,
-                //                   fontSize: 38,
-                //                 ),
-                //               )
-                //             : _timeDay == 2
-                //                 ? Text(
-                //                     'Good Afternoon! ☀️',
-                //                     style: TextStyle(
-                //                       color: blackPlus,
-                //                       fontWeight: FontWeight.bold,
-                //                       fontSize: 38,
-                //                     ),
-                //                   )
-                //                 : Text(
-                //                     'Good Evening! 🌙',
-                //                     style: TextStyle(
-                //                       color: blackPlus,
-                //                       fontWeight: FontWeight.bold,
-                //                       fontSize: 38,
-                //                     ),
-                //                   ))),
+                  SizedBox(height: 20),
 
-                SizedBox(height: 20),
-
-                ConstrainedBox(
-                  constraints: BoxConstraints(
-                    maxHeight: height*0.5, 
-                    minHeight: 0, 
-                    maxWidth: width*0.9,
-                    minWidth: width*0.9
+                  ConstrainedBox(
+                    constraints: BoxConstraints(
+                        maxHeight: height * 0.5,
+                        minHeight: 0,
+                        maxWidth: width * 0.9,
+                        minWidth: width * 0.9),
+                    child: Container(
+                      margin: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30.0),
+                        color: whitePlus,
+                      ),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
+                            child: Text(
+                              'New offers coming your way...',
+                              style: TextStyle(
+                                  color: greyPlus,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                            child: _carouselSlider(context),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                  child: Container(
-                    margin: EdgeInsets.fromLTRB(0, 5, 0, 0),
+
+                  SizedBox(height: 20),
+
+                  Container(
+                    width: width * 0.9,
+                    height: height * 0.38,
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30.0),
+                      borderRadius: BorderRadius.circular(20.0),
                       color: whitePlus,
                     ),
                     child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
                         Container(
-                          padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
+                          padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
                           child: Text(
-                            'New offers coming your way...',
+                            'Check out new tuition that you might like!',
                             style: TextStyle(
-                              color: greyPlus,
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold
-                            ),
+                                color: greyPlus,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold),
                           ),
                         ),
                         Container(
-                          padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-                          child: _carouselSlider(context),
+                          width: width * 0.9,
+                          height: height * 0.3,
+                          padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                          child: tuitionsToggle
+                              ? _loadNewTuition(widget.userData, tuitionList)
+                              : null,
                         ),
                       ],
                     ),
                   ),
-                ),
-
-                SizedBox(height: 20),
-
-                Container(
-                  width: width * 0.9,
-                  height: height * 0.38,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20.0),
-                    color: whitePlus,
-                  ),
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                        padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
-                        child: Text(
-                          'Check out new tuition that you might like!',
-                          style: TextStyle(
-                            color: greyPlus,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold
-                          ),
-                        ),
-                      ),
-                      Container(
-                        width: width * 0.9,
-                        height: height*0.3,
-                        padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-                        child: tuitionsToggle ? _loadNewTuition(widget.userData, tuitionList) : null,
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          );
-        }else{
-          return Loading();
-        }
-        
-      }
-    );
+                ],
+              ),
+            );
+          } else {
+            return Loading();
+          }
+        });
   }
 }
--- lib/screens/home-view/tuition_tile.dart
+++ lib/screens/home-view/tuition_tile.dart
@@ -1,12 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:tutorsplus/models/tuition.dart';
+import 'package:tutorsplus/screens/search-view/selected-tutor.dart';
 import 'package:tutorsplus/shared/common.dart';
+import 'package:tutorsplus/shared/transition-animations.dart';
 
 class TuitionTile extends StatelessWidget {
   TuitionTile({this.tuition,this.index});
   final Tuition tuition;
   final int index;
   
+  void _alertDialogTuitionActions({BuildContext context}) {
+
+    var alert = AlertDialog(
+      title: Text(tuition.name,
+        style: TextStyle(color: tuition.isPremium ? purplePlus : orangePlus, fontWeight: FontWeight.bold, fontSize: 20),
+      ),
+      backgroundColor: whitePlus,
+      content: RichText(
+        text: TextSpan(
+          style: TextStyle(fontSize: 16, color: greyPlus, fontWeight: FontWeight.bold),
+          children: [
+            TextSpan(text: "Description: \n"),
+            TextSpan(text: tuition.description,
+              style: TextStyle(fontSize: 14)
+            ),
+          ],
+        ),
+      ),
+      actions: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Icon( tuition.isOnline? Icons.headset_mic : Icons.headset_off ),
+        ),
+        GestureDetector(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Icon( Icons.person ),
+          ),
+          onTap: (){
+            Navigator.pop(context);
+            Navigator.push(context, ScaleToRoute(page: SelectedTutorProfile(tutorRef: tuition.tutorRef)));
+          },
+        ),
+      ],
+      elevation: 20.0,
+    );
+
+    showDialog(
+      context: context, 
+      builder: (BuildContext context) => alert,
+      barrierDismissible: true,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -29,6 +75,7 @@ class TuitionTile extends StatelessWidget {
               GestureDetector(
                 child: Icon(Icons.info,),
                 onTap: (){
+                  _alertDialogTuitionActions(context: context);
                 },
               ),
             ],
